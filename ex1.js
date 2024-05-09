683fe65078430feb5b367b71e119748bb6213a55
GIOVANNY MATHEUS CALADO ANGELO <40403420890@info.local>
2024-05-09 08:43:09 -0300

    feat:exercicio 1 de js

---
+++ ex1.js
@@ -0,0 +1,18 @@
+let username = prompt ("Digite seu nome de usuario: ");
+
+let senha = prompt("Digite a senha: ");
+
+if (username == "admin"  && senha == "senha123"){
+    console.log ("Login bem sucedido!!");
+} else {
+    console.log("Você não tem permissão de acesso")
+}
+
+// let é parecido com o Class ele dá uma variavel 
+
+// if é pra definir o correto ou o errado
+// else serve pra caso o if seja diferente do valor dado
+// && serve pra mais de um elemento tem que estar correto na operação   
+
+//alert e console.log e prompt são 3 formas de anunciar algo!
+
---
+++ exemplo.js
@@ -0,0 +1,21 @@
+//apresenta mensagem na tela
+// alert ("Hello World")
+
+// console.log("Hello World");
+
+// prompt("Digite alguma coisa: ");
+
+// let nome = prompt ("Olá, Digite o seu nome: ");
+
+// console.log ("Seja bem vindo " + nome + "!!!");
+
+// let pontos = 100;
+
+// let bonus = parseInt(prompt("Quantos pontos você deseja somar?"));
+
+// // let totalpontos = pontos + bonus;
+
+// console.log(totalpontos);
+
+// let notaAluno = parseFloat ( prompt("Digite a Nota do aluno"))
+// console.log(notaAluno )
