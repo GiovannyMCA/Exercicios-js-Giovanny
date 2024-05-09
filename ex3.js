788d605dccdaa79e14fcc984597a035251f32d6a
GIOVANNY MATHEUS CALADO ANGELO <40403420890@info.local>
2024-05-09 09:40:53 -0300

    feat: Exercicio 3

---
+++ ex3.js
@@ -0,0 +1,12 @@
+// Exercício 3: Ímpar ou par
+// Faça um programa que receba um número do usuário e informe se este número é par ou
+// ímpar.
+
+let número = parseInt(prompt ("Digite um número e eu falarei se é par ou ímpar"));
+
+if (número %2 == 0){
+    alert ("Este número é par")
+}
+else {
+    alert ("Este número é ímpar")
+}
