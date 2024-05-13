d3e7417079793f253322390f24b6db128eb57683
GIOVANNY MATHEUS CALADO ANGELO <40403420890@info.local>
2024-05-13 10:46:08 -0300

    feat: exercicio 1 loop

--- If else/ex8.js
+++ If else/ex8.js
@@ -14,28 +14,28 @@ IMC maior ou igual a 40.0: Obesidade grau 3
 */
 
 
-let altura = parseFloat (prompt ("Qual sua altura??(Metros)"))
 let peso = parseFloat (prompt ("Qual seu peso?(KG)"))
+let altura = parseFloat (prompt ("Qual sua altura??(Metros)"))
 
 let IMC = peso/(altura*altura)
 
-alert ("Seu Indice de Massa Comporal é " + IMC)
+alert ("Seu Indice de Massa Comporal é " + IMC.toFixed(2))
 
 if (IMC < 18.5){
-    alert ("Você está abaixo do peso")
+    alert ("Você está abaixo do peso") 
 }
-if (IMC > 18.6 && IMC < 24.9){
+else if (IMC > 18.6 && IMC < 24.9){
     alert ("Você está com Peso normal!!")
 }
-if (IMC > 25 && IMC < 29.9){
+else if (IMC > 25 && IMC < 29.9){
     alert ("Você está com Sobrepeso")
 }
-if (IMC > 30 && IMC < 34.9){
+else if (IMC > 30 && IMC < 34.9){
     alert ("Você está com com Obesidade grau 1 , Tome Cuidado")
 }
-if (IMC > 35 && IMC < 39.9){
+else if (IMC > 35 && IMC < 39.9){
     alert ("Você está com com Obesidade grau 2 , Tome Cuidado")
 }
-if (IMC > 39.99){
+else if (IMC > 39.99){
     alert ("Você está com com Obesidade grau 3 , Tome MUITO Cuidado!!")
 }
---
+++ for.js
@@ -0,0 +1,43 @@
+//for loop contato
+
+// for (let num = 0; num <= 10; num++){
+//     console.log (num)
+// }
+
+// for (let cont = 10; cont >= 0; cont--){
+//     console.log (cont)
+// }
+
+// // while é um loop que para quando a condição é atinjida
+
+// let salario = 1000
+
+// while (salario < 5000){
+//     salario += 100
+
+//     console.log ("seu salario é " + salario)
+// }
+
+// //do while
+
+// let saldo = 1000
+// do {
+//     console.log("Esse é o saldo atual " + saldo);
+//     saldo += 100;
+
+// }while(saldo < 5000);
+
+//array
+
+let frutas = ["banana" , "laranja" , "Maçã" , "pitaya" , "melancia" , "Caju" , "Amora" , "Melão" , "Kiwi" , "Limão"]
+
+// // console.log (frutas[3])
+
+// frutas.forEach (frutas =>{ 
+//         console.log ("A fruta é: " + frutas)
+//     })
+
+    for(let indice = 0; indice < frutas.length; indice++){
+        console.log (frutas[indice]);
+    }
+
