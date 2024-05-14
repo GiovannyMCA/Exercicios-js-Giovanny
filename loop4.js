1df64993edcfe8e8bde8f44d09bc57f6a845b7c8
GIOVANNY MATHEUS CALADO ANGELO <40403420890@info.local>
2024-05-14 08:33:37 -0300

    feat:exercicio 4 loop

---
+++ loop4.js
@@ -0,0 +1,8 @@
+// 4. Faça um programa que imprima na tela a tabuada de um número
+// qualquer escolhido pelo usuário até o 10.
+
+// número escolhido 7
+
+for (num = 7; num <= 70; num += 7){
+    console.log (num)
+}
